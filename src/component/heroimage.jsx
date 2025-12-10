08d310193e30f7bc816a50c073d955c8f6e65636
Yrieix-Cisterne-NWS <ycisterne@normandiewebschool.fr>
2025-12-10 11:35:23 +0100

    push

--- src/component/heroimage.jsx
+++ src/component/heroimage.jsx
@@ -5,7 +5,7 @@ export default function HeroImage({ hero, difficulty }) {
                 <img src={hero.image} alt={hero.name} 
                 className={difficulty == "normal" ? "blur-xl" : difficulty == "hard" ? "brightness-0" : ""}/>
             ) : (
-                <></>
+                <a>https://stats.uptimerobot.com/E0k0yU1pJQ</a>
             )}
 
         </div>
