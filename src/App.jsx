cadaf0232cc94940d8aafbdb4dc32b9f3931764e
Yrieix-Cisterne-NWS <ycisterne@normandiewebschool.fr>
2025-12-10 11:36:05 +0100

    Fixed home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import './index.css';
 import Layout from './component/layout';
 
